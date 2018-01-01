65bfe68a52311b9588572b5b8759d423ee9284eb
Steven Briggs <gshbriggs@gmail.com>
2017-12-31 22:46:26 -0500

    Create minimal API for creating and finding short URLs

--- api/main.go
+++ api/main.go
@@ -1,20 +1,78 @@
 package main
 
 import (
-	"io"
+	"crypto/sha256"
+	"encoding/base64"
 	"log"
 	"net/http"
 
 	"github.com/emicklei/go-restful"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
+var db *gorm.DB
+
+type ShortURL struct {
+	gorm.Model
+	Key   string
+	Value string
+}
+
+type CreateShortURLRequest struct {
+	URL string
+}
+
 func main() {
+	var err error
+	db, err = gorm.Open("sqlite3", "test.db")
+	if err != nil {
+		panic("Failed to connect database")
+	}
+
+	defer db.Close()
+
+	db.AutoMigrate(&ShortURL{})
+
 	ws := new(restful.WebService)
-	ws.Route(ws.GET("/hello").To(hello))
+	ws.Path("/url").
+		Consumes(restful.MIME_XML, restful.MIME_JSON).
+		Produces(restful.MIME_XML, restful.MIME_JSON)
+
+	ws.Route(ws.GET("/{key}").To(getShortURL))
+	ws.Route(ws.PUT("").To(createShortURL))
+	ws.Route(ws.POST("").To(createShortURL))
+
 	restful.Add(ws)
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func hello(req *restful.Request, resp *restful.Response) {
-	io.WriteString(resp, "world")
+func createShortURL(request *restful.Request, response *restful.Response) {
+	url := new(CreateShortURLRequest)
+	err := request.ReadEntity(&url)
+
+	if err == nil {
+		hash := sha256.New()
+		hash.Write([]byte(url.URL))
+		key := base64.URLEncoding.EncodeToString(hash.Sum(nil))
+		short := ShortURL{Key: key, Value: url.URL}
+		db.Create(&short)
+		response.WriteEntity(short)
+	} else {
+		response.WriteError(http.StatusInternalServerError, err)
+	}
+}
+
+func getShortURL(request *restful.Request, response *restful.Response) {
+	key := request.PathParameter("key")
+
+	var url ShortURL
+	err := db.First(&url, "key = ?", key).Error
+
+	if err == nil {
+		response.WriteEntity(url)
+	} else {
+		response.WriteError(http.StatusNotFound, err)
+	}
 }
