30c1063dc04576c20eda5fd5d3a380d3eec8cbc4
Steven Briggs <gshbriggs@gmail.com>
2018-01-02 22:58:39 -0500

    Add ability to use Enter key

--- ui/src/components/Home.vue
+++ ui/src/components/Home.vue
@@ -4,7 +4,7 @@
     <h2 class="subtitle">Shorten your URLs</h2>
     <div class="field is-grouped">
       <p class="control is-expanded">
-        <input v-model="url" class="input" type="url" placeholder="Enter a URL" required>
+        <input v-model="url" v-on:keyup.enter="createShortUrl" class="input" type="url" placeholder="Enter a URL" required>
       </p>
       <p class="control">
         <a v-on:click="createShortUrl" class="button is-primary">Submit</a>
