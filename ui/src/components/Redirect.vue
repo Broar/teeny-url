fef80320cf54eb23f8d3e129c6ea41a2999ac494
Steven Briggs <gshbriggs@gmail.com>
2018-01-08 22:44:24 -0500

    Add dynamic route to redirect when short URL is used

---
+++ ui/src/components/Redirect.vue
@@ -0,0 +1,24 @@
+<template>
+  <p>Redirecting you to {{fullUrl}}</p>
+</template>
+
+<script>
+  module.exports = {
+    name: 'Redirect',
+    data: function () {
+      return {
+        fullUrl: ''
+      }
+    },
+    created: function () {
+      var id = this.$route.params.id
+
+      this.$http.get('url/' + id).then(response => {
+        this.fullUrl = response.body.Value
+        window.location.replace(this.fullUrl)
+      }, response => {
+        console.error(response)
+      })
+    }
+  }
+</script>
